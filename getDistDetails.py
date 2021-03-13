6c6e3fbeb794de6fdadb8e62b40e5725b4b7b1fd
Vishal Yadav <vishalvishalyadav87@gmail.com>
2021-03-13 09:36:34 +0530

    updated getDistDetails.py script for more success ratio and to handle more conditions

--- getDistDetails.py
+++ getDistDetails.py
@@ -13,17 +13,42 @@ def findOnline(dist):
       soup = BeautifulSoup(page.content, 'html.parser')
       soup.prettify()
       info=[]
-      input_output_list = soup.find_all('tr')
-      for l in input_output_list:
-        if dist in l.get_text():
-          for el in l.get_text().split('\n'):
-            if(len(el)>0):
-              info.append(el)
-      ret = dist +' '+ info[2]+' '+info[3]+' '+info[4]+' '+info[5]
-      return ret
+      # print(soup)
+      tables = soup.find_all("table")
+      # print(tables)
+      for table in tables:
+        tbody = table.find_all("tbody")
+        if len(tbody)==0:
+          continue
+        tbody=tbody[0]
+        # print(tbody)
+        for tr in tbody.find_all("tr"):
+          # print(tr)
+          th = tr.find('th')
+          if th is None:
+            continue
+          a = th.find('a')
+          if a is None:
+            continue
+          if a.get_text() == dist:
+            for td in tr.find_all('td'):
+              temp = td.get_text().strip()
+              temp = temp.replace(',','')
+              info.append(temp)
+            if(len(info)==4):
+              ret = dist +' '+ info[0]+' '+info[1]+' '+info[2]+' '+info[3]
+            # print(ret)
+            if(len(info)==4):
+              return ret
+            else:
+              return '-1'
     except:
       return '-1'
 
 dist = sys.argv[1]
-print(findOnline(dist))
+res = findOnline(dist)
+if res is not None:
+  print(res)
+else:
+  print("-1")
   
